476d05ecd02e61fb6e03f7c66dc57d5f5da1284e
CoryBorek <agentdid127cb@aol.com>
2022-01-23 15:09:24 -0500

    Block Folder Conversion

--- bedrock-conversion/src/main/java/com/agentdid127/resourcepack/bedrock/impl/NameConverter.java
+++ bedrock-conversion/src/main/java/com/agentdid127/resourcepack/bedrock/impl/NameConverter.java
@@ -32,7 +32,13 @@ public class NameConverter extends Converter {
         if (textures_old.toFile().exists())
         Util.copyDir(textures_old, textures);
 
+        blocks(textures);
 
+
+
+    }
+
+    private void blocks(Path textures) throws IOException {
         Path blocks = textures.resolve("block");
 
         //TODO: Find what images need to be renamed, and rename them
@@ -43,6 +49,86 @@ public class NameConverter extends Converter {
             renameAll(blockMapping, ".png", textures.resolve("blocks"));
             renameAll(blockTGAMapping, ".png", ".tga", textures.resolve("blocks"));
 
+            Path candles = blocks.resolve("candles");
+
+            if (!candles.toFile().exists()) candles.toFile().mkdir();
+            moveFile(blocks, candles, "black_candle.png");
+            moveFile(blocks, candles, "black_candle_lit.png");
+            moveFile(blocks, candles, "blue_candle.png");
+            moveFile(blocks, candles, "blue_candle_lit.png");
+            moveFile(blocks, candles, "brown_candle.png");
+            moveFile(blocks, candles, "brown_candle_lit.png");
+            moveFile(blocks, candles, "candle.png");
+            moveFile(blocks, candles, "candle_lit.png");
+            moveFile(blocks, candles, "cyan_candle.png");
+            moveFile(blocks, candles, "cyan_candle_lit.png");
+            moveFile(blocks, candles, "gray_candle.png");
+            moveFile(blocks, candles, "gray_candle_lit.png");
+            moveFile(blocks, candles, "green_candle.png");
+            moveFile(blocks, candles, "green_candle_lit.png");
+            moveFile(blocks, candles, "light_blue_candle.png");
+            moveFile(blocks, candles, "light_blue_candle_lit.png");
+            moveFile(blocks, candles, "light_gray_candle.png");
+            moveFile(blocks, candles, "light_gray_candle_lit.png");
+            moveFile(blocks, candles, "lime_candle.png");
+            moveFile(blocks, candles, "lime_candle_lit.png");
+            moveFile(blocks, candles, "magenta_candle.png");
+            moveFile(blocks, candles, "magenta_candle_lit.png");
+            moveFile(blocks, candles, "orange_candle.png");
+            moveFile(blocks, candles, "orange_candle_lit.png");
+            moveFile(blocks, candles, "pink_candle.png");
+            moveFile(blocks, candles, "pink_candle_lit.png");
+            moveFile(blocks, candles, "purple_candle.png");
+            moveFile(blocks, candles, "purple_candle_lit.png");
+            moveFile(blocks, candles, "red_candle.png");
+            moveFile(blocks, candles, "red_candle_lit.png");
+            moveFile(blocks, candles, "white_candle.png");
+            moveFile(blocks, candles, "white_candle_lit.png");
+            moveFile(blocks, candles, "yellow_candle.png");
+            moveFile(blocks, candles, "yellow_candle_lit.png");
+
+            Path deepslate = blocks.resolve("deepslate");
+            if (!deepslate.toFile().exists()) deepslate.toFile().mkdir();
+            moveFile(blocks, deepslate, "chiseled_deepslate.png" );
+            moveFile(blocks, deepslate, "cobbled_deepslate.png" );
+            moveFile(blocks, deepslate, "cracked_deepslate_bricks.png" );
+            moveFile(blocks, deepslate, "cracked_deepslate_tiles.png" );
+            moveFile(blocks, deepslate, "deepslate.png" );
+            moveFile(blocks, deepslate, "deepslate_bricks.png" );
+            moveFile(blocks, deepslate, "deepslate_coal_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_copper_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_diamond_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_emerald_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_gold_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_iron_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_lapis_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_redstone_ore.png" );
+            moveFile(blocks, deepslate, "deepslate_tiles.png" );
+            moveFile(blocks, deepslate, "deepslate_top.png" );
+            moveFile(blocks, deepslate, "polished_deepslate.png" );
+
+            Path hugeFungus = blocks.resolve("huge_fungus");
+            if (!hugeFungus.toFile().exists()) hugeFungus.toFile().mkdir();
+            moveFile(blocks, hugeFungus, "crimson_door_bottom.png", "crimson_door_lower.png");
+            moveFile(blocks, hugeFungus, "crimson_door_top.png");
+            moveFile(blocks, hugeFungus, "crimson_stem.png", "crimson_log_side.png");
+            moveFile(blocks, hugeFungus, "crimson_stem_top.png", "crimson_log_top.png");
+            moveFile(blocks, hugeFungus, "crimson_planks.png");
+            moveFile(blocks, hugeFungus, "crimson_trapdoor.png");
+            moveFile(blocks, hugeFungus, "stripped_crimson_stem.png", "stripped_crimson_stem_side.png");
+            moveFile(blocks, hugeFungus, "stripped_crimson_stem_top.png");
+            moveFile(blocks, hugeFungus, "stripped_warped_stem.png", "stripped_warped_stem_side.png");
+            moveFile(blocks, hugeFungus, "stripped_warped_stem_top.png");
+            moveFile(blocks, hugeFungus, "warped_door_bottom.png", "warped_door_lower.png");
+            moveFile(blocks, hugeFungus, "warped_door_top.png");
+            moveFile(blocks, hugeFungus, "warped_planks.png");
+            moveFile(blocks, hugeFungus, "warped_stem.png", "warped_stem_side.png");
+            moveFile(blocks, hugeFungus, "warped_stem_top.png");
+            moveFile(blocks, hugeFungus, "warped_trapdoor.png");
+
+
+
+
             //grass_block_side_overlay -> grass_side
             if (textures.resolve("blocks").resolve("grass_block_side_overlay.png").toFile().exists()) {
                 ImageConverter grass = new ImageConverter(16, 16, textures.resolve("blocks").resolve("grass_block_side_overlay.png"));
@@ -50,11 +136,21 @@ public class NameConverter extends Converter {
                 System.out.println("Grass Conversion: " + grass.storeTGA());
                 Util.renameFile(textures.resolve("blocks").resolve("grass_block_side_overlay.png"), "grass_side.tga");
             }
-            }
-
-
+        }
     }
 
+    private void moveFile(Path oldDir, Path newDir, String name) throws IOException {
+        Path old = oldDir.resolve(name);
+        Path newF = newDir.resolve(name);
+
+        if (old.toFile().exists()) Files.move(old, newF);
+    }
+    private void moveFile(Path oldDir, Path newDir, String name, String newName) throws IOException {
+        Path old = oldDir.resolve(name);
+        Path newF = newDir.resolve(newName);
+
+        if (old.toFile().exists()) Files.move(old, newF);
+    }
 
 
     protected void renameAll(Mapping mapping, String extension, String newExtension, Path path) throws IOException {
